cee401297988776df26fde554a85acc8604aac98
Tijn Schmits <T.Schmits@tudelft.nl>
2014-07-19 21:51:56 +0200

    added accounts app using userena with a custom signin template

--- src/django/app/settings.py
+++ src/django/app/settings.py
@@ -1,7 +1,3 @@
-# Django settings for app project.  These are production-safe
-# settings.  Put anything that is unsafe or specific to a particular
-# deployment in local_settings.py (and do NOT check it into the repo).
-
 import os
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
@@ -26,76 +22,66 @@ DATABASES = {
     }
 }
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'key'
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
+ALLOWED_HOSTS = ['localhost','127.0.0.1','192.168.10.2']
+
 TIME_ZONE = 'UTC'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
 
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+ROOT_URLCONF = 'app.urls'
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
+WSGI_APPLICATION = 'app.wsgi.application'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+ROOT_URLCONF = 'app.urls'
 
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+WSGI_APPLICATION = 'app.wsgi.application'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    # os.path.join(PROJECT_ROOT, 'static'),
+
+# files and folders
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates')
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+)
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'change-me-gkv8x^87$#+*_m56124-&9c(0w!_-y6uh=nl9(rio30l^4o^gl'
-
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-    # 'django.template.loaders.eggs.Loader',
+)
+
+
+# backends, apps and serializers
+
+AUTHENTICATION_BACKENDS = (
+    'userena.backends.UserenaAuthenticationBackend',
+    'guardian.backends.ObjectPermissionBackend',
+    'django.contrib.auth.backends.ModelBackend',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -104,22 +90,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'app.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'app.wsgi.application'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or
-    # "C:/www/django/templates".
-
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'templates')
 )
 
 INSTALLED_APPS = (
@@ -134,15 +104,27 @@ INSTALLED_APPS = (
     'djcelery',
     'south',
     'kombu.transport.django',
+    'userena',
+    'guardian',
+    'easy_thumbnails',
+    'accounts'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+
+# email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'yourgmailaccount@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourgmailpassword'
+
+
+# logging
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -171,21 +153,21 @@ LOGGING = {
     }
 }
 
+
+# app settings
+
+# django-userena
+ANONYMOUS_USER_ID = -1
+AUTH_PROFILE_MODULE = 'accounts.Profile'
+
+# jasmine
 JASMINE_TEST_DIRECTORY = os.path.join(PROJECT_ROOT, 'static/js/')
 
-PUBLISHER_ROOT = MEDIA_ROOT
-PUBLISHER_URL = MEDIA_URL
-
+# celery
 import djcelery
 djcelery.setup_loader()
 
-########################################
-# Celery configuration
-
 CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
-# Usually it's be better to use a real queue like Redis here.  But for
-# now, with the amount of traffic we'll be pushing through the queue,
-# DB backend is more than enough.
 BROKER_URL = 'django://'
 CELERYD_MAX_TASKS_PER_CHILD = 8
 CELERY_SEND_TASK_ERROR_EMAILS = False
